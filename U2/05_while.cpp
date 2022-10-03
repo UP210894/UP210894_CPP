bb028cb3ec5946891cb13c4483491afd2839f786
UP210894 <up210894@alumnos.upa.edu.mx>
2022-10-03 10:32:02 -0500

    New

--- U2/05_while.cpp
+++ U2/05_while.cpp
@@ -1,13 +1,15 @@
 /*
-Date: 30/09/2022
+Date: 03/10/2022
 Authot: Erique Abel Herrera Vargas
 Email: up210894@alumnos.upa.edu.mx
-Description: Pizzeria menu#include <iostream>
+Description: Cycle "Do While" to ask temperature
+-----Problem-----
+Make a program in whish 6 temperatures are entered and determine the average,
+ the lowest and the highest
 */
 
 // Input/Output LIbrary between the screet "n" keyboard
 #include <iostream>
-
 
 // Namespace avoid using std:: on all input and output
 using namespace std;
@@ -15,27 +17,35 @@ using namespace std;
 //-----------MAIN FUNTION---------
 int main()
 {
-    int con = 0;
-    int contwo = 0;
+    // Declaration of variables
+    int counter = 0;
+    float temperature, temperaturesum = 0, elderly = -100000, minior = 100000;
 
-    for (int contador; contador <= 10; contador++)
-    {
-        cout << contador;
-    }
-
-    cout << endl;
-
-    while (con <= 10)
-    {
-        con += 2;
-    }
-    cout << endl;
-
+    // Cycle "Do While" to aks the temperature and determine the higuest lowest temperature
     do
     {
-        cout << contwo;
-        contwo = contwo + 2;
-    } while (contwo <= 10);
-    
+        cout << "What is the termperature ?" << endl;
+        cin >> temperature;
+        temperaturesum += temperature;
+
+        if (temperature >= elderly)
+        {
+            elderly = temperature;
+        }
+        if (temperature <= minior)
+        {
+            minior = temperature;
+        }
+
+        counter++;
+
+    } while (counter < 6);
+
+    // Print the results
+    cout << "The average temperature is: " << temperaturesum / 6 << " °C" << endl;
+    cout << "The highest temperature is: " << elderly << " °C" <<  endl;
+    cout << "The lowest temperature is: " << minior << " °C" << endl;
+
+    // Return 0 means that the program ends succesfully
     return 0;
 }
